e66c190f4369f2cf15ddeb39ed1e3c6636388e3a
Snegno <49351290+Snegno@users.noreply.github.com>
2021-04-09 18:28:06 +0300

    перезалив
    немного потрогал Вуи, затормозился на чекбоксах

--- app.js
+++ app.js
@@ -2,11 +2,32 @@
 Vue.createApp({
 	data() {
 		return {
-
-		};
+			inputText: '',
+			list_arr: [],
+			counter: 0
+		}
 	},
 	methods: {
-		
+		//получаем значение
+		inInput(event) {
+			this.inputText = event.target.value;
+		},
+		//выводим его при клике в список ТуДу
+		addTask(event) {
+			console.log('click ' + this.inputText);
+			this.list_arr.push(this.inputText);
+			console.log('li_1 = '+this.list_arr[this.counter])
+			this.inputText = '';
+			this.counter++;
+		},
+		doCheck(index, type) {
+			if(type === 'need') {
+				//мы вырезаем элемент из массива и вствляем в массив "Зделано"
+				console.log('Нарезаем массив');	
+				console.log('index = '+this.index); //выведет undefined
+				//что такое индекс
+			}
 		}
+		
 	}
 }).mount('#app');
