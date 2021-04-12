39492e0ade347414fb8536a69b813092a2700e2d
Snegno <yoko1505@yandex.ru>
2021-04-12 13:54:12 +0300

    Лист туду Готов
    Сделал Лист ТуДу на Вуе о_О

--- app.js
+++ app.js
@@ -4,29 +4,56 @@ Vue.createApp({
 		return {
 			inputText: '',
 			list_arr: [],
+			list_arr_close: [],
 			counter: 0,
-			index: 0
+			index: 0,
+			one_list_count: 0,
+			two_list_count: 0
 		}
 	},
 	methods: {
 		//получаем значение
 		inInput(event) {
+			// отлавливаем что записали в инпут
 			this.inputText = event.target.value;
 		},
 		//выводим его при клике в список ТуДу
 		addTask(event) {
-			console.log('click ' + this.inputText);
+			//добавляем задачу в массив
 			this.list_arr.push(this.inputText);
-			console.log('li_1 = '+this.list_arr[this.counter])
+			//очищаем поле и увеличиваем счетчик
 			this.inputText = '';
 			this.counter++;
-			console.log('index ' + this.index)
 		},
+		//проверяем клик по Чекбоксу
 		doCheck(item,type) {
 			if(type === 'need') {
+				//находим элемент по которому кликнули
 				this.index=this.list_arr.indexOf(item,0);
-				console.log('ты нажал на '+this.list_arr[this.index])
+				this.list_arr_close[this.list_arr_close.length] = this.list_arr[this.index]; 
+				//удаляем из первого массива скопированный эл-нт
+				this.list_arr.splice(this.index,1);
 			}
+			//если нажат чекбокс в другом списке
+			else {
+				this.index=this.list_arr_close.indexOf(item,0);
+				this.list_arr[this.list_arr.length] = this.list_arr_close[this.index]; 
+				//удаляем из второго массива скопированный эл-нт
+				this.list_arr_close.splice(this.index,1);
+				console.log('массив предстоящих задач = ' + this.list_arr)
+			}
+		},
+		removeTask(item) {
+			//находим в каком массиве есть этот элемент
+			if(this.list_arr.indexOf(item,0)>=0) {
+				this.index=this.list_arr.indexOf(item,0);
+				//удаляем из массива
+				this.list_arr.splice(this.index,1);
+			}
+			else {
+				this.index=this.list_arr_close.indexOf(item,0);
+				this.list_arr_close.splice(this.index,1)
+			}	
 		}
 		
 	}
